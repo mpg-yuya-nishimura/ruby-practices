b998e9874569053544fc9e7c586f977d51471696
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-22 19:22:27 +0900

    点数を整数に変換する処理の追加

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,9 +1,17 @@
 class Shot
   def initialize(score)
-    @score = score
+    @score = parse_shot_result(score)
   end
 
   def score
     @score
+  end
+
+  private
+
+  def parse_shot_result(score)
+    return 10 if score == 'X'
+
+    score.to_i
   end
 end
