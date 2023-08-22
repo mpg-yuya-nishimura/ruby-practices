09740a45d84d05dbbaa7a40e511c6841fd67bb6c
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-22 18:21:35 +0900

    フレームごとに採点結果を求める処理を追加

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -29,3 +29,22 @@ all_shots.each do |shot|
 end
 
 all_frames = frames_score_points.map { |frames_score_point| Frame.new(frames_score_point) }
+
+frame_result_points = []
+all_frames.each_with_index do |frame, i|
+  frame_result_points << if frame.frames_score_points.length == 1
+                           if frames_score_points[i + 1] && frames_score_points[i + 2] && frames_score_points[i + 1].size == 1
+                             frame.frames_score_points + [frames_score_points[i + 1][0]] + [frames_score_points[i + 2][0]]
+                           else
+                             frame.frames_score_points + frames_score_points[i + 1]&.first(2)
+                           end
+                         elsif frame.frames_score_points.length == 2
+                           if frame.frames_score_points.sum == STRIKE_SCORE
+                             frame.frames_score_points << frames_score_points[i + 1][0]
+                           else
+                             frame.frames_score_points
+                           end
+                         else
+                           frame.frames_score_points
+                         end
+end
