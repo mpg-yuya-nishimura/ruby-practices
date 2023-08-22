32027bc977cfa00ed748eaeb7c04a494bc4631b3
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-22 19:24:59 +0900

    ストライク・スペアの出方に応じて、前後のフレームを参照し、フレームに持たる値を作成する

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -5,17 +5,16 @@
 require 'debug'
 require './shot.rb'
 require './frame.rb'
+require './game.rb'
 
 STRIKE_SCORE = 10
 TOTAL_GAME_COUNT = 10
 
-parsed_score_numbers = ARGV[0].split(',').map { |s| s == 'X' ? 10 : s.to_i }
-
-all_shots = parsed_score_numbers.map { |parsed_score_number| Shot.new(parsed_score_number) }
+shots = ARGV[0].split(',').map { |score| Shot.new(score) }
 
 frames_score_points = []
 tmp_scores = []
-all_shots.each do |shot|
+shots.each do |shot|
   if tmp_scores.empty? && shot.score == STRIKE_SCORE && frames_score_points.size < TOTAL_GAME_COUNT - 1
     frames_score_points << [shot.score]
     tmp_scores = []
@@ -28,23 +27,27 @@ all_shots.each do |shot|
   end
 end
 
-all_frames = frames_score_points.map { |frames_score_point| Frame.new(frames_score_point) }
-
 frame_result_points = []
-all_frames.each_with_index do |frame, i|
-  frame_result_points << if frame.frames_score_points.length == 1
+frames_score_points.each_with_index do |frame_scores, i|
+  frame_result_points << if frame_scores.length == 1
                            if frames_score_points[i + 1] && frames_score_points[i + 2] && frames_score_points[i + 1].size == 1
-                             frame.frames_score_points + [frames_score_points[i + 1][0]] + [frames_score_points[i + 2][0]]
+                             frame_scores + [frames_score_points[i + 1][0]] + [frames_score_points[i + 2][0]]
                            else
-                             frame.frames_score_points + frames_score_points[i + 1]&.first(2)
+                             frame_scores + frames_score_points[i + 1]&.first(2)
                            end
-                         elsif frame.frames_score_points.length == 2
-                           if frame.frames_score_points.sum == STRIKE_SCORE
-                             frame.frames_score_points << frames_score_points[i + 1][0]
+                         elsif frame_scores.length == 2
+                           if frame_scores.sum == STRIKE_SCORE
+                             frame_scores << frames_score_points[i + 1][0]
                            else
-                             frame.frames_score_points
+                             frame_scores
                            end
                          else
-                           frame.frames_score_points
+                           frame_scores
                          end
 end
+
+frames = frame_result_points.map { |frame_result_point| Frame.new(frame_result_point) }
+result_frames_scores = frames.map { |frame| frame.frame_result_points }
+
+game = Game.new(result_frames_scores)
+game.show_total_score
