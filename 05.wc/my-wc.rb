d02565dbbba35dbae78afa1c6b8fde06e0e08f51
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-24 20:18:32 +0900

    オプションによって処理を変更するための記述を追加

---
+++ 05.wc/my-wc.rb
@@ -0,0 +1,9 @@
+require 'optparse'
+require 'etc'
+
+opt = OptionParser.new
+options = {}
+opt.on('-l') { |v| options[:l] = v }
+opt.on('-w') { |v| options[:w] = v }
+opt.on('-c') { |v| options[:c] = v }
+opt.parse!(ARGV)
