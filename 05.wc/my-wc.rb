6bb5685ed922fb552235159344ce9bd2acd908c8
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-24 21:15:13 +0900

    wcコマンドのファイルパスを一つ与えた時の動作の作成

--- 05.wc/my-wc.rb
+++ 05.wc/my-wc.rb
@@ -14,8 +14,6 @@ opt.on('-w') { |v| options[:w] = v }
 opt.on('-c') { |v| options[:c] = v }
 opt.parse!(ARGV)
 
-# binding.break
-
 filename = ARGV[0]
 
 unless filename && File.exist?(filename)
@@ -27,10 +25,18 @@ line_count = 0
 word_count = 0
 byte_count = 0
 
-File.open(filename, "r") do |file|
-  file.each_line do |line|
-    line_count += 1
-    word_count += word_count.split(/\s+/).size
-    byte_count += line.bytesize
-  end
+
+file = File.open(filename, "r")
+
+file.each_line do |line|
+  line_count += 1
+  word_count += line.split(/\s+/).size
+  byte_count += line.bytesize
 end
+
+count_result_text = ''
+count_result_text += line_count.to_s.rjust(8) if options[:l] || options.empty?
+count_result_text += word_count.to_s.rjust(8) if options[:w] || options.empty?
+count_result_text += byte_count.to_s.rjust(8) if options[:c] || options.empty?
+
+puts count_result_text
