819e6a7ba5b686b7ac402d7e1a9bacd109e98784
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-09-04 16:03:32 +0900

    複数ファイルが引数として与えられた時に、対応できるように変更

--- 05.wc/my-wc.rb
+++ 05.wc/my-wc.rb
@@ -7,6 +7,10 @@ require 'etc'
 
 require 'debug'
 
+def file_exist?(filenames)
+  filenames.all? { |filename| File.exist?(filename) }
+end
+
 opt = OptionParser.new
 options = {}
 opt.on('-l') { |v| options[:l] = v }
@@ -14,29 +18,35 @@ opt.on('-w') { |v| options[:w] = v }
 opt.on('-c') { |v| options[:c] = v }
 opt.parse!(ARGV)
 
-filename = ARGV[0]
+if ARGV.empty?
+  puts 'ファイルの指定がありません。'
+end
 
-unless filename && File.exist?(filename)
+unless file_exist?(ARGV)
   puts '指定のファイルは存在しません'
-  exit
 end
 
-line_count = 0
-word_count = 0
-byte_count = 0
+integrated_count_results = ARGV.map do |filename|
+  line_count = 0
+  word_count = 0
+  byte_count = 0
 
+  file = File.open(filename, "r")
 
-file = File.open(filename, "r")
+  file.each_line do |line|
+    line_count += 1
+    word_count += line.split(/\s+/).size
+    byte_count += line.bytesize
+  end
 
-file.each_line do |line|
-  line_count += 1
-  word_count += line.split(/\s+/).size
-  byte_count += line.bytesize
+  [line_count, word_count, byte_count, filename]
 end
 
-count_result_text = ''
-count_result_text += line_count.to_s.rjust(8) if options[:l] || options.empty?
-count_result_text += word_count.to_s.rjust(8) if options[:w] || options.empty?
-count_result_text += byte_count.to_s.rjust(8) if options[:c] || options.empty?
+integrated_count_results.each do |integrated_count_result|
+  count_result_text = ''
+  count_result_text += integrated_count_result[0].to_s.rjust(8) if options[:l] || options.empty?
+  count_result_text += integrated_count_result[1].to_s.rjust(8) if options[:w] || options.empty?
+  count_result_text += integrated_count_result[2].to_s.rjust(8) if options[:c] || options.empty?
 
-puts count_result_text
+  puts "#{count_result_text} #{integrated_count_result[3]}"
+end
