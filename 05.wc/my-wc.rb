5108b9c197a49888d9bf0cc25a95c01ad937b736
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-09-05 15:13:51 +0900

    ファイルのカウント情報のテキスト生成を共通化するため

--- 05.wc/my-wc.rb
+++ 05.wc/my-wc.rb
@@ -11,6 +11,15 @@ def file_exist?(filenames)
   filenames.all? { |filename| File.exist?(filename) }
 end
 
+def create_result_text(stats, options)
+  total_stats_text = ''
+  total_stats_text += stats[0].to_s.rjust(8) if options[:l] || options.empty?
+  total_stats_text += stats[1].to_s.rjust(8) if options[:w] || options.empty?
+  total_stats_text += stats[2].to_s.rjust(8) if options[:c] || options.empty?
+
+  total_stats_text
+end
+
 opt = OptionParser.new
 options = {}
 opt.on('-l') { |v| options[:l] = v }
@@ -18,7 +27,9 @@ opt.on('-w') { |v| options[:w] = v }
 opt.on('-c') { |v| options[:c] = v }
 opt.parse!(ARGV)
 
-if ARGV.empty? && $stdin.tty?
+stdin_data = $stdin
+
+if ARGV.empty? && stdin_data.tty?
   puts 'ファイルの指定がありません。'
   exit
 end
@@ -28,8 +39,7 @@ unless file_exist?(ARGV)
   exit
 end
 
-stdin_data = $stdin.read
-filenames = ARGV.empty? ? [stdin_data] : ARGV
+filenames = ARGV.empty? ? [stdin_data.read] : ARGV
 
 integrated_count_results = filenames.map do |filename|
   line_count = 0
@@ -50,10 +60,7 @@ end
 total_stats = [0, 0, 0]
 
 integrated_count_results.each do |integrated_count_result|
-  count_result_text = ''
-  count_result_text += integrated_count_result[0].to_s.rjust(8) if options[:l] || options.empty?
-  count_result_text += integrated_count_result[1].to_s.rjust(8) if options[:w] || options.empty?
-  count_result_text += integrated_count_result[2].to_s.rjust(8) if options[:c] || options.empty?
+  count_result_text = create_result_text(integrated_count_result, options)
 
   total_stats[0] += integrated_count_result[0]
   total_stats[1] += integrated_count_result[1]
@@ -64,10 +71,7 @@ integrated_count_results.each do |integrated_count_result|
 end
 
 if ARGV.size > 1
-  total_stats_text = ''
-  total_stats_text += total_stats[0].to_s.rjust(8) if options[:l] || options.empty?
-  total_stats_text += total_stats[1].to_s.rjust(8) if options[:w] || options.empty?
-  total_stats_text += total_stats[2].to_s.rjust(8) if options[:c] || options.empty?
+  total_stats_text = create_result_text(total_stats, options)
 
   puts "#{total_stats_text} total"
 end
