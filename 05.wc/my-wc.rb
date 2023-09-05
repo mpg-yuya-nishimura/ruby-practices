6bd64a2c625d3544d0ac91b3870b0ad55e13d9a6
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-09-05 15:48:05 +0900

    短いコードで書くためにリファクタリング

--- 05.wc/my-wc.rb
+++ 05.wc/my-wc.rb
@@ -5,19 +5,15 @@
 require 'optparse'
 require 'etc'
 
-require 'debug'
-
 def file_exist?(filenames)
   filenames.all? { |filename| File.exist?(filename) }
 end
 
 def create_result_text(stats, options)
-  total_stats_text = ''
-  total_stats_text += stats[0].to_s.rjust(8) if options[:l] || options.empty?
-  total_stats_text += stats[1].to_s.rjust(8) if options[:w] || options.empty?
-  total_stats_text += stats[2].to_s.rjust(8) if options[:c] || options.empty?
-
-  total_stats_text
+  text = ''
+  text += stats[0].to_s.rjust(8) if options[:l] || options.empty?
+  text += stats[1].to_s.rjust(8) if options[:w] || options.empty?
+  text += stats[2].to_s.rjust(8) if options[:c] || options.empty?
 end
 
 opt = OptionParser.new
@@ -27,51 +23,41 @@ opt.on('-w') { |v| options[:w] = v }
 opt.on('-c') { |v| options[:c] = v }
 opt.parse!(ARGV)
 
-stdin_data = $stdin
+original_argv = ARGV.dup
 
-if ARGV.empty? && stdin_data.tty?
-  puts 'ファイルの指定がありません。'
-  exit
-end
-
-unless file_exist?(ARGV)
+unless ARGF
   puts '指定のファイルは存在しません'
   exit
 end
 
-filenames = ARGV.empty? ? [stdin_data.read] : ARGV
+unless file_exist?(original_argv)
+  puts '指定のファイルは存在しません'
+  exit
+end
 
-integrated_count_results = filenames.map do |filename|
+file_stats = ARGV.map do |filename|
   line_count = 0
   word_count = 0
   byte_count = 0
 
-  text = ARGV.empty? ? stdin_data : File.open(filename, 'r')
+  file = File.open(filename, 'r')
 
-  text.each_line do |line|
+  file.each_line do |line|
     line_count += 1
-    word_count += line.split(/\s+/).size
+    word_count += line.split.size
     byte_count += line.bytesize
   end
 
   [line_count, word_count, byte_count, filename]
 end
 
+file_stats.each { |file_stat| puts "#{create_result_text(file_stat, options)} #{file_stat[3]}" }
+
 total_stats = [0, 0, 0]
-
-integrated_count_results.each do |integrated_count_result|
-  count_result_text = create_result_text(integrated_count_result, options)
-
-  total_stats[0] += integrated_count_result[0]
-  total_stats[1] += integrated_count_result[1]
-  total_stats[2] += integrated_count_result[2]
-
-  total_result_text = integrated_count_result[3] unless ARGV.empty?
-  puts "#{count_result_text} #{total_result_text}"
+ARGF.each do |line|
+  total_stats[0] += 1
+  total_stats[1] += line.split.size
+  total_stats[2] += line.bytesize
 end
 
-if ARGV.size > 1
-  total_stats_text = create_result_text(total_stats, options)
-
-  puts "#{total_stats_text} total"
-end
+puts "#{create_result_text(total_stats, options)} total" unless original_argv.size == 1
