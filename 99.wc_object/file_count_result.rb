290b642c59d7c0488329e6fcfead23149e693cf5
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-02-26 20:17:04 +0900

    不要なインスタンス変数を削除

--- 99.wc_object/file_count_result.rb
+++ 99.wc_object/file_count_result.rb
@@ -4,20 +4,17 @@ class FileCountResult
   attr_reader :text
 
   def initialize(file, options)
-    @file_name = file[:name] || 'total'
-    @line_count = file[:line_count].to_s
-    @word_count = file[:word_count].to_s
-    @byte_count = file[:byte_count].to_s
-    @text = create_result_text(options)
+    @text = create_result_text(file, options)
   end
 
   private
 
-  def create_result_text(options)
+  def create_result_text(file, options)
     text = ''
-    text += @line_count.rjust(8) if options[:l] || options.empty?
-    text += @word_count.rjust(8) if options[:w] || options.empty?
-    text += @byte_count.rjust(8) if options[:c] || options.empty?
-    "#{text} #{@file_name}"
+    text += file[:line_count].to_s.rjust(8) if options[:l] || options.empty?
+    text += file[:word_count].to_s.rjust(8) if options[:w] || options.empty?
+    text += file[:byte_count].to_s.rjust(8) if options[:c] || options.empty?
+    file_name = file[:name] || 'total'
+    "#{text} #{file_name}"
   end
 end
