a7aa296ff23d96c65914e92a1f080941c4cda8f3
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-02-26 17:13:23 +0900

    指定されたファイルの存在性を検証するためのメソッドを実装

--- 99.wc_object/file_stats_analyzer.rb
+++ 99.wc_object/file_stats_analyzer.rb
@@ -6,6 +6,7 @@ class FilesStatsAnalyzer
   def initialize
     extract_options
     @original_argv = ARGV.dup
+    validate_files_accessible
   end
 
   def display
@@ -20,5 +21,17 @@ class FilesStatsAnalyzer
     opt.on('-r') { |v| @options[:r] = v }
     opt.on('-l') { |v| @options[:l] = v }
     opt.parse!(ARGV)
+  end
+
+  def validate_files_accessible
+    unless ARGF
+      puts '指定のファイルは存在しません'
+      exit
+    end
+
+    unless all_files_exist?
+      puts '指定のファイルは存在しません'
+      exit
+    end
   end
 end
